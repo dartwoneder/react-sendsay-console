515a744ead97bf5286d1d0922ba70e7dcac95370
Dart Wonder <alf.xyz.dev@gmail.com>
2020-06-01 17:31:00 +0300

    Heroky deploy

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
                 <Route path="/login">
                   <LoginPage />
                 </Route>
-                <PrivateRoute path="/console">
+                <PrivateRoute path="/">
                   <ConsolePage />
                 </PrivateRoute>
               </Switch>
